39cbafae9a3b6b27731f9c28b551739d58039dbb
Adrian Gutierrez <adgutierrez@ikeasistencia.com>
2023-01-14 09:52:44 -0600

    Cambios archivos drive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+var access_token; 
+function inClient() {
+    const client = google.accounts.oauth2.initTokenClient({
+        client_id: '749662371198-vgqiahi6cqfg079i2n2k0nase6tcm7st.apps.googleusercontent.com',
+        scope: 'https://www.googleapis.com/auth/drive.readonly',
+        callback: (response) => {
+         access_token = response.access_token;
+        },
+      });
+    client.requestAccessToken();
+
+    
+}
+
+
 function showColocacion(element){
     
     const padre = element.parentNode;
@@ -186,85 +201,62 @@ function closeSession(){
     sessionStorage.clear();
 }
 
-//Funcion que sirve para graficar y leer csv, falta leer archivo desde drive
-function leerCsv(){
-    let angulos = [];
-    let labels = [];
-    Papa.parse("../assets/out.csv",{
-        download: true,
-        header: true,
-        skipEmptyLines: true,
-        complete: function(results){
-            
-            console.log(results);
-            results.data.forEach(element => {
-                angulos.push(parseFloat(element.Angulos).toFixed(2));
-                labels.push(parseFloat(element.Tiempo).toFixed(2));
+async function hola(){
+    
+    //console.log(GoogleAuth);
+    //APIKEY AIzaSyAWcWx3H-TNL-pEXd_dtnGdjZ21hsBF5e8
+    // CLIENTID 749662371198-vgqiahi6cqfg079i2n2k0nase6tcm7st.apps.googleusercontent.com
+    //https://www.googleapis.com/drive/v3/files?q=name%3D%20%27res.csv%27&key=AIzaSyAWcWx3H-TNL-pEXd_dtnGdjZ21hsBF5e8
+    let peticion = await fetch('https://www.googleapis.com/drive/v3/files?q=name%3D%20%27out.csv%27&key=AIzaSyAWcWx3H-TNL-pEXd_dtnGdjZ21hsBF5e8',{
+        headers: {
+            'Authorization': 'Bearer ' + access_token
+        }
+    })
+    .then((res) =>  res.json())
+    .then((data) =>  data.files[0].id)
+    .then(async (id) => {
+        let peticion2 = await fetch('https://www.googleapis.com/drive/v3/files/'+id+'?alt=media&key=AIzaSyAWcWx3H-TNL-pEXd_dtnGdjZ21hsBF5e8')
+        .then((res) => { return res.blob(); })
+        .then((data) => {
+            let angulos = [];
+            let labels = [];
+            Papa.parse(data,{
+            download: true,
+            header: true,
+            skipEmptyLines: true,
+            complete: function(results){
+                
+                console.log(results);
+                results.data.forEach(element => {
+                    angulos.push(parseFloat(element.Angulos).toFixed(2));
+                    labels.push(parseFloat(element.Tiempo).toFixed(2));
             });
 
-            
-        }
-    });
-
-    const ctx = document.getElementById('myChart');
-    console.log(labels);
-    console.log(angulos);
-    let myChart = new Chart(ctx,{
-        type:"line",
-        data:{
-            labels: labels,
-            datasets:[{
-                label: "Angulos",
-                data: angulos,
-                fill: false,
-            }]
-        }
-    });
-
-}
-
-async function hola(){
-    //https://www.googleapis.com/drive/v3/files/{fileId}?alt=media&key={APIKey}
-    //https://www.googleapis.com/drive/v3/files/1CIeB0C8k5exJvAOC6g-Vb4Vo9eHoCRqG
-    let peticion = await fetch('https://www.googleapis.com/drive/v3/files/1CIeB0C8k5exJvAOC6g-Vb4Vo9eHoCRqG?alt=media&key=AIzaSyAWcWx3H-TNL-pEXd_dtnGdjZ21hsBF5e8')
-    .then((res) => { return res.blob(); })
-    .then((data) => {
-        let angulos = [];
-        let labels = [];
-        Papa.parse(data,{
-        download: true,
-        header: true,
-        skipEmptyLines: true,
-        complete: function(results){
-            
-            console.log(results);
-            results.data.forEach(element => {
-                angulos.push(parseFloat(element.Angulos).toFixed(2));
-                labels.push(parseFloat(element.Tiempo).toFixed(2));
-        });
-
-            
-        }
-        });
-        const ctx = document.getElementById('myChart');
-        console.log(labels);
-        console.log(angulos);
-        let myChart = new Chart(ctx,{
-            type:"line",
-            data:{
-                labels: labels,
-                datasets:[{
-                    label: "Angulos",
-                    data: angulos,
-                    fill: false,
-                }]
+                
             }
+            });
+            const ctx = document.getElementById('myChart');
+            console.log(labels);
+            console.log(angulos);
+            let myChart = new Chart(ctx,{
+                type:"line",
+                data:{
+                    labels: labels,
+                    datasets:[{
+                        label: "Angulos",
+                        data: angulos,
+                        fill: false,
+                    }]
+                }
+            });
         });
-    });
+    })
+
 
 }
 
-hola();
+
+inClient();
 isSessionOn();
 //leerCsv();
 
